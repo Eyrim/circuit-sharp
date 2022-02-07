391b00e50c7e2af662b70fe888e988ca92f4a09d
Eyrim <marfxedbot@gmail.com>
2022-02-07 17:00:46 +0000

    Began working on editor window in JS, written comments to document future plans

---
+++ wwwroot/js/Editor/TrackMouse.js
@@ -0,0 +1,11 @@
+﻿function drawComponent(mousePosX, mousePosY, componentID) {
+    // The div representing the active schematic area
+    let componentContainer = document.getElementById(""); //TODO: Populate
+
+    // The translucent outline of the component, showing hovering
+    let componentGhost = document.createElement("span");
+    componentGhost.className(componentID);
+    componentGhost.style.top = mousePosY + "px";
+    componentGhost.style.left = mousePosX + "px";
+    componentGhost.id = componentID + GetNumberOfComponents();
+}
