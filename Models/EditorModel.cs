ffbd180e3f67c2abe740c7513a79b8d42028b449
Eyrim <marfxedbot@gmail.com>
2022-03-11 15:56:30 +0000

    Removed some files from SchematicEditor, need to clear up references to old DLL; have IComponent, then make Component factory that takes in enum for type, then based on enum use polymorphism to create components with the right typed parameters

--- Models/EditorModel.cs
+++ Models/EditorModel.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CircuitSharp.Util.CSV;
+using Ksu.Cis300.Graphs;
 
 namespace CircuitSharp.Models
 {
     public class EditorModel
     {
-        private string TypeID;
-        private string UUID;
-        private string Position;
-        private string Connections;
+        
     }
 }
