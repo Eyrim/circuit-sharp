3417a58c014078eae3d2489d07eb63e1759ebba1
Eyrim <marfxedbot@gmail.com>
2022-03-16 15:00:50 +0000

    Playing around with pointers, messaged Mark and waiting for a reply

--- Models/EditorModel.cs
+++ Models/EditorModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CircuitSharp.SchematicEditor.src.Components;
+using CircuitSharp.SchematicEditor.src.Components.Resistors;
 using CircuitSharp.Util.CSV;
 using Ksu.Cis300.Graphs;
 
@@ -12,10 +13,20 @@ namespace CircuitSharp.Models
     {
         private static DirectedGraph<Component, int> _ComponentGraph;
 
-        public void AddNodeToTree(Component ToAdd)
+        public static List<Component> ComponentPointerList { get; set; }
+
+        public static void AddNodeToTree(Component ToAdd)
         {
             _ComponentGraph.AddNode(ToAdd);
-            _ComponentGraph.TryGetEdge(); //TODO: Work out way to identify each node properly, maybe use a pointer? i dont want to do that omg omg pls no :c   41
+            unsafe
+            {
+                GenericResistor r = new GenericResistor();
+
+                GenericResistor* grp = *r;
+
+                ComponentPointerList.Add(r);
+            }
+             //TODO: Work out way to identify each node properly, maybe use a pointer? i dont want to do that omg omg pls no :c   41
         }
     }
 }
