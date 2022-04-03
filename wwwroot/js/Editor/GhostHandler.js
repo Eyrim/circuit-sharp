f3ac99c6d26c1d6611ce480a3b972151e2089a4e
Eyrim <marfxedbot@gmail.com>
2022-04-03 20:46:33 +0100

    Finally fixed the JS for GhostHandler.js, I'm a moron. Still got issues with the CSS on the table not working properly

--- wwwroot/js/Editor/GhostHandler.js
+++ wwwroot/js/Editor/GhostHandler.js
@@ -51,72 +51,6 @@ window.onload = () => {
             }
         }
 
-
-        // https://www.geeksforgeeks.org/remove-all-the-child-elements-of-a-dom-node-in-javascript/#:~:text=Child%20nodes%20can%20be%20removed,which%20produces%20the%20same%20output. theft :D
-        const removeAllChildren = function (parentID) {
-            var e = document.getElementById(parentID);
-
-            //e.firstElementChild can be used.
-            var child = e.firstElementChild;
-            while (child) {
-                e.removeChild(child);
-                child = e.firstElementChild;
-            }
-        }
-
-        const attachMouseMoveHandlers = function (els) {
-            let testID = 0;
-            let currentlyOver;
-            let previouslyOver;
-            let drawnOver;
-            let drawn = false;
-
-            $("#activeSchematicAreaTable").mousemove((event) => {
-                event.stopPropagation();
-                if (event.target.id != "activeSchematicAreaTable" && event.target.tagName != "IMG") {
-                    console.log("Target ID: " + event.target.id);
-                    //currentlyOver = event.target.id;
-                    if (!(drawn)) {
-                        removeAllChildren(event.target.id);
-                        // Draw component
-                        drawComponent(event.target.id, testID);
-                        drawn = true;
-                        drawnOver = event.target.id;
-                    }
-
-                    if (drawnOver != event.target.id) {
-                        // Remove component
-                        removeAllChildren(event.target.id);
-                        populateTableRows();
-                        //removeComponent(event.target.id, testID);
-                        drawn = false;
-                    }
-                }
-            })
-        }
-
-        const removeComponent = function (id, toRemove) {
-            let parent = document.getElementById(id);
-            let children = parent.children;
-            //toRemove -= 1;
-
-            for (let i = 0; i < children.length; i++) {
-                if (children[i].id == toRemove) {
-                    parent.removeChild(children[i]);
-                    console.log("removed component");
-                    return;
-                }
-            }
-        }
-
-        const sleep = function (milliseconds) {
-            const date = Date.now();
-            let currentDate = null;
-            do {
-                currentDate = Date.now();
-            } while (currentDate - date < milliseconds);
-        } /* https://www.sitepoint.com/delay-sleep-pause-wait/ */
-
         const drawComponent = function (id, toAdd) {
             let container = document.getElementById(id);
             let el = document.createElement('img');
@@ -131,74 +65,27 @@ window.onload = () => {
             console.log("appended component");
         }
 
+        const enterHandler = function (event) {
+            drawComponent(event.target.id, "testID");
+            console.log("Appended To: " + event.target.id);
+        }
 
-        populateTableRows();
-        attachMouseMoveHandlers(getTableRows());
+        const leaveHandler = function (event) {
+            document.getElementById(event.target.id).removeChild(document.getElementById("testID"));
+        }
+
+        const attachHandlers = function () {
+            // Attaches mousemove listeners
+            for (let i = 0; i <= 24; i++) {
+                //document.getElementById(i).addEventListener("mousemove", moveHandler);
+                document.getElementById(i).addEventListener("mouseenter", enterHandler);
+                document.getElementById(i).addEventListener("mouseleave", leaveHandler);
+            }
+        }
+
+
+
+        attachHandlers();
 
     })
 }
-
-/* Fiddle
- * 
- * const removeAllChildren = function(parentID) {
-  var e = document.getElementById(parentID);
-
-  //e.firstElementChild can be used.
-  var child = e.firstElementChild;
-  while (child) {
-    e.removeChild(child);
-    child = e.firstElementChild;
-  }
-}
-const drawComponent = function(parentID, IDToAdd) {
-  let container = document.getElementById(parentID);
-  let el = document.createElement('img');
-  let url = `https://www.w3.org/People/mimasa/test/imgformat/img/w3c_home.png`;
-
-  el.src = url;
-  el.id = IDToAdd;
-  el.style.zIndex = "9999999";
-  el.style.filter = "opacity(50%)";
-
-  container.appendChild(el);
-  console.log("appended component");
-}
-
-$("#activeSchematicAreaTable").mousemove((event) => {
-  event.stopPropagation();
-  console.log("Mouse moved on: " + event.target.id);
-
-  let currentElementInData;
-  let lastDrawnOn;
-
-  // If mouse moved on table data
-  if (event.target.id != "activeSchematicAreaTable" && event.target.tagName != "IMG") {
-    // Save current element in data
-    currentElementInData = event.target.children[0];
-
-    // Remove current element in data
-    removeAllChildren(event.target.id);
-
-    // Draw hover element in data
-    drawComponent(event.target.id, "ghost");
-    lastDrawnOn = event.target;
-
-    // Wait for mouse click
-    event.target.addEventListener("mouseClick", mouseClickCallback);
-
-    // Draw hover element properly
-    var mouseClickCallback = function() {
-      console.log("TODO: Draw component properly");
-    }
-
-    // Wait for mouse to move out of that cell
-    if (event.target.id !== lastDrawnOn.id) {
-      // Remove current element in old cell
-      removeAllChildren(lastDrawnOn);
-      // Draw old element saved
-      lastDrawnOn.appendChild(currentElementInData);
-    }
-  }
-});
-
- */
